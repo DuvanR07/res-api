b7df8e8b2648db70c5c64e5e9232df24fd0ec223
Duvan Rosero <duvanrosero1995@gmail.com>
2019-05-31 09:32:30 -0500

    Proyecto con dependencias inicializado y configurado

---
+++ src/test/java/com/restaurante/resapi/ResApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.restaurante.resapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ResApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
